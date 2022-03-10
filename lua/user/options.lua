2fc8409b8f7e4dde45eb75b63ffe4523390a95fb
Samuel <42911064+lyb200@users.noreply.github.com>
2022-03-10 19:52:07 +0800

    add some my config

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,19 +1,21 @@
 local options = {
   backup = false,                          -- creates a backup file
   clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
   -- colorcolumn = "80",
   colorcolumn = "120",
   fileencoding = "utf-8",                  -- the encoding written to a file
   hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
+  incsearch = true,
   mouse = "a",                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showmode = true,                         -- we don't need to see things like -- INSERT -- anymore
   showtabline = 2,                         -- always show tabs
+  ignorecase = true,                       -- ignore case in search patterns
   smartcase = true,                        -- smart case
+  autoindent = true,
   smartindent = true,                      -- make indenting smarter again
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
@@ -28,13 +30,34 @@ local options = {
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
-  relativenumber = false,                  -- set relative numbered lines
+  relativenumber = true,                   -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+
+  -- my configure
+  backspace = "indent,eol,start",
+  undolevels = 5000,
+  ttyfast = true,
+  lazyredraw = true,
+  textwidth = 0,
+  startofline = true,                      -- move the cursor to the first non-blank of the line
+  visualbell = true,                       -- use visual bell instead of beeping
+  virtualedit = "block",                   -- allow virtual editing in Visual block mode
+  autochdir = true,                        -- can change directory
+  shiftround = true,                       -- round indent to multiple of 'shiftwidth'
+  list = true,                             -- show tab and tailable
+  history = 1000,                          -- keep 1000 items in the history
+  ruler = true,
+  showcmd = true,
+  timeout = false,
+  ttimeoutlen = 0,
+  foldlevel = 99,
+  foldenable = true,
+
 }
 
 vim.opt.shortmess:append "c"
@@ -43,6 +66,29 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
+--vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd "set whichwrap+=<,>,[,]"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+vim.cmd [[set listchars=tab:\|\ ,trail:▫]]
+vim.cmd [[set diffopt+=vertical]]          -- show diffs side by side
+vim.cmd [[let &t_CO=256]]
+
+vim.cmd [[
+  if has('linebreak')
+    " indent wrapped lines to match start
+    set linebreak
+    "Arrow pointing downwards then curving rightwards
+    let &showbreak='⤷' "⤷(U+2937)
+    if exists('&breakindentopt')
+      "emphasize broken line by indenting them
+      set breakindentopt=shift:2
+    endif
+  endif
+]]
+
+vim.cmd [[
+  " open file where you leave the file
+  autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+]]
